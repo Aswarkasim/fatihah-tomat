5ed1b8c1014368f88f70c86217191ac0909b7ba4
unknown <aswarkasim@gmail.com>
2023-02-23 06:28:16 +0700

    23

--- resources/views/admin/diagnosa/result.blade.php
+++ resources/views/admin/diagnosa/result.blade.php
@@ -50,7 +50,7 @@
       
                   <tr>
                     <td>{{$loop->iteration}}</td>
-                    <td>{{isset($item->gejeala) ? $item->gejala->tanggal : ''}}</td>
+                    <td>{{isset($item->gejala) ? $item->gejala->tanggal : ''}}</td>
                     <td>{{$item->nilai_cf}}</td>
                
                   </tr>
