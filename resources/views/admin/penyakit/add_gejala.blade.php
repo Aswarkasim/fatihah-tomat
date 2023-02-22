838d37b965c751b71a2a49c0c30b24c690a956b0
unknown <aswarkasim@gmail.com>
2023-02-23 06:40:35 +0700

    23

--- resources/views/admin/penyakit/add_gejala.blade.php
+++ resources/views/admin/penyakit/add_gejala.blade.php
@@ -41,7 +41,7 @@
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-          <button type="submit" class="btn btn-primary">Upload</button>
+          <button type="submit" class="btn btn-primary">Tambah</button>
         </div>
       </form>
     </div>
